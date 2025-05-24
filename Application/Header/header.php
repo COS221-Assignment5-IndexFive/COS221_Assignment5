0f7cf9558ef5c15ce5b4449756164d4b84011332
jonty282 <u23536862@tuks.co.za>
2025-05-24 22:35:19 +0200

    Adding navbar

---
+++ Application/Header/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="top-nav">
+  <div class="nav-wrapper">
+    <a href="/Application/CustomerView/php/customer.php" class="nav-logo">ShopSmart</a>
+    <nav class="nav-links">
+      <a href="/Application/CustomerView/php/customer.php">Products</a>
+      <a href="#">Comparison</a>
+      <a href="/Application/WatchlistView/php/watchlist.php">Watchlist</a>
+      <a href="/logout.php" class="logout-btn">Logout</a>
+    </nav>
+  </div>
+</header>
+
+</body>
+</html>
