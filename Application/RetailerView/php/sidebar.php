d9296f82b808a9fe67f1f76a6d1d31d68395a801
g3rard-j <jordaangerard10@gmail.com>
2025-05-26 22:26:33 +0200

    Retailer logout and session clear

---
+++ Application/RetailerView/php/sidebar.php
@@ -0,0 +1,11 @@
+  <aside class="sidebar glass-card">
+      <div class="brand">
+          <a href="index.php"><span class="material-icons">dashboard</span>
+          <h1>CompareIt Retailer</h1></a>
+      </div>
+      <nav>
+          <ul>
+              <li id="logout"><a href="#"><span class="material-icons">logout</span> Logout</a></li>
+          </ul>
+      </nav>
+  </aside>
